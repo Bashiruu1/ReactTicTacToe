77e660a9a6e5a0eee3779c7fef19251fdd9fbc38
Bashiruu1 <silvergold21@gmail.com>
2021-07-02 21:15:53 -0400

    moved calculate to bottom

--- src/index.js
+++ src/index.js
@@ -100,6 +100,13 @@ class Game extends React.Component {
   }
 }
 
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -119,10 +126,3 @@ function calculateWinner(squares) {
   }
   return null;
 }
-// ========================================
-
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
-
